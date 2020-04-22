32f89f690b65f8e63ba5a7cf76955ec7dadd6aee
fritzgt <tuanis@live.com>
2020-04-22 13:37:47 +0300

    Initial Commit

---
+++ AVLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVLesson
+//
+//  Created by FGT MAC on 4/22/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
